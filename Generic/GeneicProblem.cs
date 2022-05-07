740b482ec3e91bc76c2ae9b2d68d2bcca3d46863
mohan338 <sahumohan338@gmail.com>
2022-05-07 11:51:15 +0530

    [mohan]add:maximum integer number

---
+++ Generic/GeneicProblem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class GeneicProblem
+    {
+        public void MaxInt(int[] arr)
+        {
+            foreach(var item in arr)
+            {
+                Console.WriteLine(item+" ");
+            }
+            Array.Sort(arr);
+            Console.WriteLine("max element is " + arr[arr.Length - 1]);
+        }
+    }
+}
---
+++ Generic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        GeneicProblem problem = new GeneicProblem();
+        int[] arr = { 15, 20, 18, 47, 68, 61, 5 };
+        problem.MaxInt(arr);
+    }
+}
